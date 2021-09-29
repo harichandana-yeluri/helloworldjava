0a3bd371e23f04109e482cbc6157e930bef0400e
harichandana-yeluri <51232180+harichandana-yeluri@users.noreply.github.com>
2021-09-29 11:35:04 +0530

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,7 @@
 public class helloworldjava {
    public static void main(String[] args) {
     System.out.println("Hello World from Jenkins DevOps course!!");
+    System.out.println("This is a test for poll");
     }
 }
 
